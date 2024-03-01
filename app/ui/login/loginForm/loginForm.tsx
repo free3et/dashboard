e0983a6c3315af454dbef878d4189f741764a2e3
free3et <free3et@gmail.com>
2024-03-01 11:41:57 +0200

    update google btn

--- app/ui/login/loginForm/loginForm.tsx
+++ app/ui/login/loginForm/loginForm.tsx
@@ -2,14 +2,17 @@
 import React from "react";
 import styles from "./loginForm.module.css";
 import { useFormState } from "react-dom";
-import { authenticate } from "../../../lib/actions";
-import { GoogleButton } from "./GoogleButton";
+import { authenticate, googleAuthenticate } from "../../../lib/actions";
 
 const LoginForm = () => {
   const [state, formAction] = useFormState(authenticate, undefined);
+  const [errorMsgGoogle, dispatchGoogle] = useFormState(
+    googleAuthenticate,
+    undefined
+  );
 
   return (
-    <>
+    <div className={styles.wrapper}>
       <form action={formAction} className={styles.form}>
         <h2 className={styles.title}>Fill in your username and password</h2>
         <input type="text" placeholder="username" name="username" />
@@ -17,9 +20,11 @@ const LoginForm = () => {
         <button>Login</button>
 
         {state && state}
-        {/*   <GoogleButton /> */}
       </form>
-    </>
+      <form className={styles.googleform} action={dispatchGoogle}>
+        <button>Google Sign In</button>
+      </form>
+    </div>
   );
 };
 
